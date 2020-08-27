dfaa1a8d67cc9979b0f8e531a899b8c05648dd7b
amarbessedik <amar.bessedik@gmail.com>
2020-08-27 18:34:56 -0400

    Mobile responsiveness update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,28 @@ import React from 'react';
 import ShareButtons from "./ShareButtons";
 import styles from './Footer.module.css';
 
+const center ={
+  textAlign: 'center'
+}
+
 function Footer() {
     return (
       <div className={styles.footer}>
         <ul className={styles.footer__list}>
+          <li style={center}>
+            <h3 style={{ padding: 0, marginBottom: "5px" }}>
+              &copy; COVID-19 GLOBL TRACKER 2020{" "}
+              {!new Date().getFullYear() === 2020
+                ? " - " + new Date().getFullYear()
+                : null}
+            </h3>
+          </li>
+          <li style={{ textTransform: "uppercase" }} style={center}>
+            <span>Designed &amp; Developed by Amar Bessedik</span>
+          </li>
           <li>
-            <h3 style={{padding:0, marginBottom: '5px'}}>&copy; COVID-19 GLOBL TRACKER.</h3>
+            <span style={center}>amar.bessedik@gmail.com </span>
           </li>
-          <li style={{ textTransform: "uppercase" }}>
-            Amar Bessedik, August 2020{" "}
-            {!new Date().getFullYear() === 2020
-              ? " - " + new Date().getFullYear()
-              : null}
-          </li>
-          <li>amar.bessedik@gmail.com</li>
         </ul>
         <ul className={styles.footer__list}>
           <li>
